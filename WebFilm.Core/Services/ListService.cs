9865f113917cae3b62a5fd1652fb9542b6c33a21
viethoan261 <lb26012001@gmail.com>
2023-04-28 20:37:44 +0700

    done

--- WebFilm.Core/Services/ListService.cs
+++ WebFilm.Core/Services/ListService.cs
@@ -262,7 +262,7 @@ namespace WebFilm.Core.Services
 
             int totalCount = comments.Count();
             int totalPages = (int)Math.Ceiling((double)totalCount / paging.pageSize);
-            comments = comments.Skip((paging.pageIndex - 1) * paging.pageSize).OrderByDescending(p => p.CreatedDate).Take(paging.pageSize);
+            comments = comments.OrderByDescending(p => p.CreatedDate).Skip((paging.pageIndex - 1) * paging.pageSize).Take(paging.pageSize);
             comments = comments.ToList();
             List<BaseCommentDTO> commentDTOs = new List<BaseCommentDTO>();
             foreach (Comment comment in comments)
