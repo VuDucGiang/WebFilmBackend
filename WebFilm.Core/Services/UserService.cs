9b1d075341077916c0b62aa025e5428a9761be3b
viethoan261 <lb26012001@gmail.com>
2023-05-01 18:53:53 +0700

    fix api profile

--- WebFilm.Core/Services/UserService.cs
+++ WebFilm.Core/Services/UserService.cs
@@ -480,29 +480,34 @@ namespace WebFilm.Core.Services
             //rate stat
             RateStat rateStats= new RateStat();
             List<RateStatDTO> rateStatsPopular = _reviewRepository.GetRatesByUserID(userID).ToList();
-            List<float> ratesValue = new List<float>();
-            for (float i = 1; i <= 10; i++)
+            if (rateStatsPopular.Count > 0)
             {
-                ratesValue.Add(i/2f);
-            }
-            foreach (RateStatDTO statDTO in rateStatsPopular) {
-                if (ratesValue.Contains(statDTO.Value))
+                List<float> ratesValue = new List<float>();
+                for (float i = 1; i <= 10; i++)
                 {
-                    ratesValue.Remove(statDTO.Value);
+                    ratesValue.Add(i / 2f);
                 }
+                foreach (RateStatDTO statDTO in rateStatsPopular)
+                {
+                    if (ratesValue.Contains(statDTO.Value))
+                    {
+                        ratesValue.Remove(statDTO.Value);
+                    }
+                }
+
+                foreach (float rate in ratesValue)
+                {
+                    RateStatDTO newRate = new RateStatDTO();
+                    newRate.Value = rate;
+                    rateStatsPopular.Add(newRate);
+                }
+
+
+                rateStats.List = rateStatsPopular;
+                rateStats.Total = rateStatsPopular.Select(p => p.Total).Sum();
+                rateStats.RateAverage = rateStatsPopular.Select(p => p.Value * p.Total).Sum() / rateStats.Total;
             }
-
-            foreach(float rate in ratesValue)
-            {
-                RateStatDTO newRate = new RateStatDTO();
-                newRate.Value = rate;
-                rateStatsPopular.Add(newRate);
-            }
-
-
-            rateStats.List = rateStatsPopular;
-            rateStats.Total = rateStatsPopular.Select(p => p.Total).Sum();
-            rateStats.RateAverage = rateStatsPopular.Select(p => p.Value * p.Total).Sum() / rateStats.Total;
+            
 
             // recent like review
             List<BaseReviewDTO> recentLikesReview = new List<BaseReviewDTO>();
