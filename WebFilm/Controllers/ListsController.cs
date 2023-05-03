599e950fec127911a42bf0ebc167eb3f70974941
VuDucGiang <ducgiang.nd1808@gmail.com>
2023-05-03 13:16:04 +0700

    add

--- WebFilm/Controllers/ListsController.cs
+++ WebFilm/Controllers/ListsController.cs
@@ -39,7 +39,7 @@ namespace WebFilm.Controllers
         }
 
         [Authorize]
-        [HttpPost("AddList")]
+        [HttpPost("Add")]
         public async Task<IActionResult> AddListDetail([FromBody] ListDTO list)
         {
             try
