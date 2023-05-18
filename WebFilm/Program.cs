8102935bda9ebaa230e2fb43e2f42b0945eb41d7
viethoan261 <lb26012001@gmail.com>
2023-05-18 09:33:43 +0700

    add all origin

--- WebFilm/Program.cs
+++ WebFilm/Program.cs
@@ -100,7 +100,8 @@ builder.Services.AddCors(options =>
                               policy.WithOrigins("http://localhost:5173")
                                                       .AllowAnyHeader()
                                                       .AllowAnyMethod();
-
+                              policy.AllowAnyOrigin().AllowAnyHeader()
+                                                      .AllowAnyMethod();
                           });
    
 });
